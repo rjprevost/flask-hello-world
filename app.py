6f58de92ab44ea3c7f208e4156bf2c32674a439c
Reuben Prevost <reubenprevost@gmail.com>
2015-06-28 03:26:42 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 #----------------- Flask Hello World -----------------------#
 
+# import the Flask class from the flask module
 from flask import Flask
 
+#create the application object
 app = Flask(__name__)
-'''
+
+# error handling
+app.config["DEBUG"] = True
+
+# dynamic routes
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1 
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "reuben":
+		return "hello, {}".format(name)
+	else :
+		return "Not Found", 404
+
+#use the decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
+#define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World!"
-'''
+	return "Hello, World!!?!?!?!?!"
+
+#start the development server using the run() method
 if __name__ == "__main__":
 	app.run()
